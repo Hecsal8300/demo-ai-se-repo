1b2328b380d60fde0fa4b9f3b0a6dbb59c958c8e
hecsal8300 <hector.8300@gmail.com>
2026-01-30 13:42:55 -0500

    initial commit

---
+++ NA.js
@@ -0,0 +1,8 @@
+function utility(str) {
+    console.log("Length:", str.length);
+    console.log("Uppercase:", str.toUpperCase());
+    console.log("Reversed:", str.split("").reverse().join(""));
+}
+
+// function call
+utility("Masood");
